db6e8a3ec99c3c39b5bf36ec70c4eae978ad24c1
Kelley Buhlig <155096006+Kelferd7@users.noreply.github.com>
2024-07-03 08:45:29 -0500

    Update propulsion.py

--- propulsion.py
+++ propulsion.py
@@ -2,26 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Constants/Inputs
-mass_flow_rate = 100  # kg/s
-exit_velocity = 300  # m/s
-initial_velocity = 100  # m/s
-exit_area = 0.1  # m^2
-exit_pressure = 100000  # Pa
-ambient_pressure = 101325  # Pa
+massFlowRate = 100  # kg/s
+exitVelocity = 300  # m/s
+initialVelocity = 100  # m/s
+exitArea = 0.1  # m^2
+exitPressure = 100000  # Pa
+ambientPressure = 101325  # Pa
 
 # Time parameters
 time = np.linspace(0, 100, 500)  # 100 seconds, 500 intervals
 
 # Thrust Calculation
-thrust = mass_flow_rate * (exit_velocity - initial_velocity) + exit_area * (exit_pressure - ambient_pressure)
+thrust = massFlowRate * (exitVelocity - initialVelocity) + exitArea * (exitPressure - ambientPressure)
 
 # Simplified Fuel Consumption
-fuel_consumption_rate = 0.5  # kg/s
-fuel_consumption = fuel_consumption_rate * time
+fuelConsumptionRate = 0.5  # kg/s
+fuelConsumption = fuelConsumptionRate * time
 
 # Plotting the results
 plt.figure(figsize=(10, 5))
 
+# Thrust vs Time plot
 plt.subplot(1, 2, 1)
 plt.plot(time, thrust * np.ones_like(time), label='Thrust')
 plt.xlabel('Time (s)')
@@ -29,8 +30,9 @@ plt.ylabel('Thrust (N)')
 plt.title('Thrust vs Time')
 plt.legend()
 
+#Fuel Consumption vs Time plot
 plt.subplot(1, 2, 2)
-plt.plot(time, fuel_consumption, label='Fuel Consumption', color='orange')
+plt.plot(time, fuelConsumption, label='Fuel Consumption', color='orange')
 plt.xlabel('Time (s)')
 plt.ylabel('Fuel Consumed (kg)')
 plt.title('Fuel Consumption vs Time')
